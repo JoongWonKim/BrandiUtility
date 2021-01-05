8336b331fe1ae5719a3ca698f3a682380b544f66
kimjw2 <kimjw2@brandi.co.kr>
2021-01-05 17:31:45 +0900

    test

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BrandiUtility",
-            dependencies: ["RealmSwift", "SDWebImage"],
+            dependencies: ["Realm", "SDWebImage"],
             path: "Sources"),
         .testTarget(
             name: "BrandiUtilityTests",
