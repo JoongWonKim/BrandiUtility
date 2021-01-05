82345d4e8a8e628dfba335852e9aa4220556e6f6
kimjw2 <kimjw2@brandi.co.kr>
2021-01-05 16:58:17 +0900

    test

--- Package.swift
+++ Package.swift
@@ -2,7 +2,6 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
-import class Foundation.ProcessInfo
 
 let package = Package(
     name: "BrandiUtility",
@@ -15,27 +14,19 @@ let package = Package(
             name: "BrandiUtility",
             targets: ["BrandiUtility"]),
     ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa", from: "10.5.0"),
+        .package(name: "SDWebImage", url: "https://github.com/SDWebImage/SDWebImage", from: "5.10.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BrandiUtility",
-            dependencies: ["Realm", "SDWebImage"],
-            path: "Sources"),
+            dependencies: ["Realm", "SDWebImage"]),
         .testTarget(
             name: "BrandiUtilityTests",
             dependencies: ["BrandiUtility"]),
     ]
 )
-
-if ProcessInfo.processInfo.environment["SWIFTCI_USE_LOCAL_DEPS"] == nil {
-    package.dependencies += [
-        .package(name: "Realm", url: "https://github.com/realm/realm-cocoa", from: "10.5.0"),
-        .package(name: "SDWebImage", url: "https://github.com/SDWebImage/SDWebImage", from: "5.10.0"),
-    ]
-} else {
-    package.dependencies += [
-        .package(path: "../Realm"),
-        .package(path: "../SDWebImage"),
-    ]
-}
