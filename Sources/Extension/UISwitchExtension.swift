42284b9c0a611a0a0cb4182ba259bf2e6370f21d
kimjw2 <kimjw2@brandi.co.kr>
2021-01-06 16:35:20 +0900

    test

---
+++ Sources/Extension/UISwitchExtension.swift
@@ -0,0 +1,19 @@
+//
+//  UISwitch+Extension.swift
+//  Brandi
+//
+//  Created by Joo Hee Kim on 21. 01. 04..
+//  Copyright © 2021 Brandi. All rights reserved.
+//
+
+import UIKit
+
+extension UISwitch {
+    func set(width: CGFloat, height: CGFloat) {
+        let standardHeight: CGFloat = 31
+        let standardWidth: CGFloat = 51
+        let heightRatio = height / standardHeight
+        let widthRatio = width / standardWidth
+        transform = CGAffineTransform(scaleX: widthRatio, y: heightRatio)
+    }
+}
