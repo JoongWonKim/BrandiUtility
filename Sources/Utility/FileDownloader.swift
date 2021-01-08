bfff2c9c3bb902557f7b208135179a4924a5e4c8
kimjw2 <kimjw2@brandi.co.kr>
2021-01-08 12:45:38 +0900

    test

--- Sources/Utility/FileDownloader.swift
+++ Sources/Utility/FileDownloader.swift
@@ -6,26 +6,25 @@
 //  Copyright © 2016년 Brandi. All rights reserved.
 //
 
-//import Foundation
-//import Alamofire
-//
-//open class FileDownloader {
-//    public static func loadFileFromUrl(_ url: URL, completion: ((_ success: Bool, _ data: Data?) -> Void)?) {
-//        let sessionConfig = URLSessionConfiguration.default
-//        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
-//        var request = URLRequest(url: url)
-//        request.httpMethod = "GET"
-//        let task = session.dataTask(with: request, completionHandler:
-//            { data, response, error in
-//                if error == nil {
-//                    //success
-//                    completion?(true, data)
-//                } else {
-//                    //error
-//                    completion?(false, nil)
-//                }
-//            }
-//        )
-//        task.resume()
-//    }
-//}
+import Foundation
+
+open class FileDownloader {
+    public static func loadFileFromUrl(_ url: URL, completion: ((_ success: Bool, _ data: Data?) -> Void)?) {
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        let task = session.dataTask(with: request, completionHandler:
+            { data, response, error in
+                if error == nil {
+                    //success
+                    completion?(true, data)
+                } else {
+                    //error
+                    completion?(false, nil)
+                }
+            }
+        )
+        task.resume()
+    }
+}
